b9d37d3b4893be1349e5cef51a9440a73b8cdd94
Naswan Nashir <naswannashir.r@gmail.com>
2025-10-27 10:12:17 +0700

    update

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
+echo Compiling for Windows...
+
 echo Compiling server.cpp...
-g++ -std=c++11 -o server.exe src/server.cpp src/des.cpp -lws2_32
+g++ -std=c++11 -Wall -Isrc -o server.exe src/server.cpp src/des.cpp -lws2_32
+
 echo Compiling client.cpp...
-g++ -std=c++11 -o client.exe src/client.cpp src/des.cpp -lws2_32
+g++ -std=c++11 -Wall -Isrc -o client.exe src/client.cpp src/des.cpp -lws2_32
+
 echo Done.
